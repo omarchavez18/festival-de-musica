11d21c5f477ca4b1367a6c01b62b773f21e82a14
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-01-22 17:40:32 -0600

    creacion archivo js y funcion para llamar las imagenes para la galeria

---
+++ build/js/app.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  iniciarApp();
+});
+
+function iniciarApp() {
+  crearGaleria();
+}
+
+// esta funcion sirve para llamar las imagenes de la carpeta y generar una galeria de imagenes
+function crearGaleria() {
+  const galeria = document.querySelector(".galeria-imagenes");
+
+  for (let i = 1; i <= 12; i++) {
+    const imagen = document.createElement("picture");
+    imagen.innerHTML = `<source srcset="build/img/thumb/${i}.avif" type="image/avif" />
+        <source srcset="build/img/thumb/${i}.webp" type="image/webp" />
+        <img
+          loading="lazy"
+          width="200"
+          height="300"
+          src="build/img/thumb/${i}.jpg"
+          alt="imagen galeria"
+        />`;
+    galeria.appendChild(imagen);
+  }
+}
