f8c457ae8e1c97070f9109af7ee26448a85cae70
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-01-25 01:05:55 -0600

    barra de navegacion fija

--- build/js/app.js
+++ build/js/app.js
@@ -3,7 +3,42 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function iniciarApp() {
+  navegacionFija();
   crearGaleria();
+  scrollNav();
+}
+
+// BARRA DE MENU FIJA AUNQUE DEMOS SCROLL.
+function navegacionFija() {
+  const barra = document.querySelector(".header");
+  const sobreFestival = document.querySelector(".sobre-festival");
+  const body = document.querySelector("body");
+
+  window.addEventListener("scroll", function () {
+    if (sobreFestival.getBoundingClientRect().top < 0) {
+      barra.classList.add("fijo");
+      body.classList.add("body-scroll");
+    } else {
+      barra.classList.remove("fijo");
+      body.classList.remove("body-scroll");
+    }
+  });
+}
+
+//SMOOTH SCROLL.
+//esta funcion nos permite dar smooth scroll hacia los enlaces del menu, osea baja lentamente
+function scrollNav() {
+  const enlaces = document.querySelectorAll(".navegacion-principal a");
+
+  enlaces.forEach((enlace) => {
+    enlace.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const seccionScroll = e.target.attributes.href.value;
+      const seccion = document.querySelector(seccionScroll);
+      seccion.scrollIntoView({ behavior: "smooth" });
+    });
+  });
 }
 
 // esta funcion sirve para llamar las imagenes de la carpeta y generar una galeria de imagenes
